1f0a0ed7c22ae58ca4e9eec02c5397f5c02ca0d8
용주 엄 <aishlove369@hotmail.com>
2020-04-18 00:42:51 +0900

    .

--- CSharpStudy/000StudyTest/obj/Release/netcoreapp2.2/000StudyTest.AssemblyInfo.cs
+++ CSharpStudy/000StudyTest/obj/Release/netcoreapp2.2/000StudyTest.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("001DeepCopy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("000StudyTest")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("001DeepCopy")]
-[assembly: System.Reflection.AssemblyTitleAttribute("001DeepCopy")]
+[assembly: System.Reflection.AssemblyProductAttribute("000StudyTest")]
+[assembly: System.Reflection.AssemblyTitleAttribute("000StudyTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
