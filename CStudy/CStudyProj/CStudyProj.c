7e7fad94bc8577ebb175730cd0983b5f098b5518
용주 엄 <aishlove369@hotmail.com>
2020-05-22 15:21:45 +0900

    20200522 study

--- CStudy/CStudyProj/CStudyProj.c
+++ CStudy/CStudyProj/CStudyProj.c
@@ -1,6 +1,18 @@
 ﻿#include <stdio.h>
+#include <stdlib.h> // malloc(), free()
 
-int main()
+int main(int argc, char* argv[])
 {
-    printf("Hello World!\n");
+	int num1 = 32;
+	int num2 = 7;
+	float num3;
+
+	num3 = num1 / num2;		// 컴파일 경고 발생
+	printf("%f\n", num3);	// 4.000000 (뒷자리가 날아갔음)
+
+	num3 = (float)num1 / num2;	// num1 을 float로 변환
+	printf("%f\n", num3);		// 4.571429
+
+	return 0;
+
 }
