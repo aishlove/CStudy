ed9e2ad3b71ac5f16227315ca83df6250f6773b3
용주 엄 <aishlove369@hotmail.com>
2020-05-28 21:31:22 +0900

    2020-05-28

---
+++ CStudy/28.10/28.10.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	unsigned char i = 1;
+	while (i *= 2 && i <= 128)
+	{
+		printf("%u\n", i);
+		i <<= 1;
+	}
+	return 0;
+}
---
+++ CStudy/Dojang-28.5/Dojang-28.5.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int count;
+
+	scanf("%d", &count);
+
+	int i = 0;
+	while (count > 0)
+	{
+		printf("Hello, World! %d\n", count);
+		count--;
+	}
+	return 0;
+}
