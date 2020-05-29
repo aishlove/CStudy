1737cd9ba807ed1fe78a633bc94485eac977b621
용주 엄 <aishlove369@hotmail.com>
2020-05-29 16:03:51 +0900

    2020-05-29

---
+++ CStudy/Dojang-21.6/Dojang-21.6.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/* if 조건문 뿐만 아니라, 삼항연산자에서도 논리 연산자를 사용할 수 있다. */
+
+int main()
+{
+	int num1 = 1;
+	int num2 = 0;
+
+	printf("%s\n", num1 && num2 ? "참" : "거짓");	
+	// 거짓 : 삼항연산자에서 AND연산자 사용.
+
+	printf("%s\n", num1 || num2 ? "참" : "거짓");
+	// 참 : 삼항연산자에서 OR 연산자 사용.
+
+	return 0;
+
+}
---
+++ CStudy/Dojang-21.9/Dojang-21.9.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/* 표준 입력으로 두 정수가 입력됩니다.
+	다음 조건대로 입력된 두 정수(1, 0)를 검사하여 결과를 출력하는 프로그램을 만드세요.
+	scanf 함수 호출 전에 문자열을 출력하면 안됩니다.
+*/
+
+int main()
+{
+	/*int num1;
+	int num2;
+
+	scanf("%d %d", &num1, &num2);
+	printf("%s\n", num1 && num2 ? "참" : "거짓");
+	printf("%s\n", num1 || num2 ? "참" : "거짓");
+	printf("%s\n", num1 != 1 ? "참" : "거짓");
+
+	return 0;*/
+
+
+	int num1;
+	int num2;
+
+	scanf("%d %d", &num1, &num2);
+	
+	if (num1 && num2)
+		printf("%s\n", "참");
+	else
+		printf("%s\n", "거짓");
+
+	if (num1 || num2)
+		printf("%s\n", "참");
+	else
+		printf("%s\n", "거짓");
+
+	if (!num1)
+		printf("%s\n", "참");
+	else
+		printf("%s\n", "거짓");
+
+	return 0;
+
+}
