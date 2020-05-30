4afa8e15485aac9ed21ec6c48824b34199721469
용주 엄 <aishlove369@hotmail.com>
2020-05-30 16:11:23 +0900

    2020-05-30

--- CStudy/Dojang-31.2/Dojang-31.2.c
+++ CStudy/Dojang-31.2/Dojang-31.2.c
@@ -5,14 +5,114 @@ int main()
 {
 	// 31.2   사각형으로 별 출력하기
 
-	for (int i = 0; i < 5; i++)
+	/*for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 50; j++)
+		{
+			printf("&");
+		}
+		printf("\n");
+	}*/
+
+
+	// 31.3   계단식으로 별 출력하기
+
+	/*for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 5; j++)
 		{
-			printf("*");
+			if (j <= i)
+				printf("*");
 		}
 		printf("\n");
+	}*/
+
+
+	// 31.4   대각선으로 별 출력하기
+
+	/*for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (j == i)
+				printf("*");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}*/
+
+	/* 실제로는 한번에 생각하는게 아니라 <, >, <=, =>를 번갈아서 넣어보며
+	결과를 확인하는 경우가 많으므로 이해가 되지 않는다고 초조해할 필요는 없다...! 
+	난 이해했닥우 ^^ */
+
+
+	// 31.6   연습문제 : 역삼각형 모양으로 별 출력하기
+
+	/*for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (j < i)
+				printf(" ");
+			else
+				printf("*");
+		}
+	}*/
+
+
+
+	// 31.7   심사문제 : 산 모양으로 별 출력하기
+
+	// 입력 3
+
+	//int count = 0;
+	//scanf("%d", &count);
+
+	//// int i = count;	// count값을 int i 에 복사.
+
+	//for (int i = 0; i <= count; i++)
+	//{
+	//	int count2 = ((count * i) + 2);
+	//	for (int j = 0; j < count2 ; j++)
+	//	{
+	//		if (j < i)
+	//			printf(" ");
+
+	//		else if (j == i)
+	//			printf("*");
+
+	//		else
+	//			printf(" ");
+	//	}
+	//	printf("\n");
+	//}
+
+	int height;
+	scanf("%d", &height);
+	
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = height - 1; j >= 0; j--)
+		{
+			if (j > i)
+				printf(" ");
+			else
+				printf("*");
+		}
+		// 여기까지 삼각형 왼쪽
+
+		for (int j = 0; j < height - 1; j++)
+		{
+			if (j < i)
+				printf("*");
+		}
+		printf("\n");
+		// 삼각형 오른쪽
 	}
+
+	
 
 	return 0;	
 }
+
---
+++ CStudy/Dojang-32.1/Dojang-32.1.cpp
@@ -0,0 +1,100 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+	// 32.1   goto와 레이블 사용하기
+
+	/*int num1;
+
+	scanf("%d", &num1);
+
+	if (num1 == 1)
+		goto ONE;
+	else if (num1 == 2)
+		goto TWO;
+	else
+		goto EXIT;
+
+
+ONE:
+	printf("1입니다.\n");
+	goto EXIT;
+
+TWO:
+	printf("2입니다.\n");
+	goto EXIT;
+
+EXIT:
+	return 0;*/
+
+
+
+	// 32.2   중첩 루프 빠져나오기
+
+	//int num1 = 0;
+
+	//bool exitOuterLoop = false;	// 바깥쪽 루프를 빠져나올지 결정하는 변수 f로 초기화
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	for (int j = 0; j < 10; j++)
+	//	{
+	//		if (num1 == 20)
+	//		{
+	//			exitOuterLoop = true;
+	//			break;
+	//		}
+	//		num1++;
+	//	}
+
+	//	if (exitOuterLoop == true)
+	//		break;
+	//}
+
+	//printf("%d\n", num1);
+
+	//return 0;
+
+	/* break의 가장 큰 특징은 현재 루프만 끝낸다는 점이다.
+	   따라서 중첩 루프의 안쪽 루프에서 break를 사용하면
+	   안쪽 루프만 끝낼 뿐 바깥쪽 루프는 계속 반복된다.
+	   
+	   그러다보니, 여기서는 변수 exitOuterLoop를 사용해서
+	   바깥쪽 루프도 끝내겠다고 결정을 해주고 있다.
+	   
+	   이처럼 중첩 루프를 빠져나오려면 추가적인 코드가 더 필요한데,
+	   goto를 사용하면 간단히 빠져나올 수 있습니다.*/
+
+
+
+
+	int num1 = 0;
+
+	for (int i = 0; i<10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				if(num1 ==20)
+					goto EXIT;
+
+				num1++;
+			}
+		}
+	EXIT : 
+	printf("%d\n", num1);	// 20
+
+	return 0;
+
+
+	/* 이처럼 goto는 다중루프를 빠져나올 때 유용합니다.
+	   여기서는 for 반복문을 예로 들었지만 
+	   while, dowhile로 된 중첩루프도 빠져나올 수 있습니다.
+	   
+	   또한 에러 상황을 한데에 EXIT: 레이블쪽에 모아서
+	   중복 코드를 정리하는 기능 또한 수행 가능하다.
+	   
+	  */
+	
+}
