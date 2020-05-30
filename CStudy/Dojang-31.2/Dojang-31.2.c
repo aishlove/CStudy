0c0b66bf7fd5ffac125e4b501ad461206779ccd9
용주 엄 <aishlove369@hotmail.com>
2020-05-30 14:21:33 +0900

    2020-05-30

---
+++ CStudy/Dojang-31.2/Dojang-31.2.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	// 31.2   사각형으로 별 출력하기
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;	
+}
